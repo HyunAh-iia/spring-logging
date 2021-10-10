48e0d37a36ce362b7d44ed11dceb1d4e71b8dea7
iia <kha2464@gmail.com>
2021-10-10 20:51:56 +0900

    HTTP Request/Response 로깅

---
+++ src/main/java/my/study/springlogging/global/filters/ResponseWrapper.java
@@ -0,0 +1,11 @@
+package my.study.springlogging.global.filters;
+
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class ResponseWrapper extends ContentCachingResponseWrapper {
+    public ResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+}
