cd1ca41c6600bddd507aef50575889633af3d0c4
iia <kha2464@gmail.com>
2021-10-10 20:44:25 +0900

    init

---
+++ src/test/java/my/study/springlogging/SpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.springlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
